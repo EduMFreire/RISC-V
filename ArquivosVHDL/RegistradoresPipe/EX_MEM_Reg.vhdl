72123a154e5370636e1ec6c6b46779178e987b0f
PedroRubinstein <pedrovrubinstein.20231@poli.ufrj.br>
2025-06-19 12:28:34 -0300

    Organizing wires, removing unecessary signals

--- ArquivosVHDL/RegistradoresPipe/EX_MEM_Reg.vhdl
+++ ArquivosVHDL/RegistradoresPipe/EX_MEM_Reg.vhdl
@@ -12,15 +12,10 @@ entity EX_MEM_Pipeline is
         MemWrite_in  : in  std_logic;
         MemToReg_in  : in  std_logic;
         RegWrite_in  : in  std_logic;
-        branch_in    : in  std_logic;
-        jump_in      : in  std_logic;
         -- Data signals
         ALUResult_in : in  std_logic_vector(31 downto 0);
         WriteData_in : in  std_logic_vector(31 downto 0);
         Rd_in       : in  std_logic_vector(4 downto 0);
-        zero_in       : in  std_logic;
-        branch_target_in : in  std_logic_vector(31 downto 0);
-
         bubble       : in std_logic; -- Entrada de bubble síncrona
 
         -- Outputs to MEM stage
@@ -30,11 +25,7 @@ entity EX_MEM_Pipeline is
         RegWrite_out : out std_logic;
         ALUResult_out: out std_logic_vector(31 downto 0);
         WriteData_out: out std_logic_vector(31 downto 0);
-        Rd_out       : out std_logic_vector(4 downto 0);
-        branch_out   : out std_logic;
-        jump_out     : out std_logic;
-        zero_out     : out std_logic;
-        branch_target_out : out std_logic_vector(31 downto 0)
+        Rd_out       : out std_logic_vector(4 downto 0)
     );
 end EX_MEM_Pipeline;
 
@@ -50,10 +41,6 @@ begin
             ALUResult_out <= (others => '0');
             WriteData_out <= (others => '0');
             Rd_out   <= (others => '0');
-            jump_out      <= '0';
-            branch_out    <= '0';
-            zero_out      <= '0';
-            branch_target_out <= (others => '0');
         elsif rising_edge(clk) then
             if bubble = '1' then
 
@@ -64,10 +51,6 @@ begin
                 ALUResult_out <= (others => '0');
                 WriteData_out <= (others => '0');
                 Rd_out   <= (others => '0');
-                jump_out      <= '0';
-                branch_out    <= '0';
-                zero_out      <= '0';
-                branch_target_out <= (others => '0');
 
             else
                 MemRead_out   <= MemRead_in;
@@ -77,10 +60,6 @@ begin
                 ALUResult_out <= ALUResult_in;
                 WriteData_out <= WriteData_in;
                 Rd_out   <= Rd_in;
-                jump_out      <= jump_in;
-                branch_out    <= branch_in;
-                zero_out      <= zero_in;
-                branch_target_out <= branch_target_in;
             end if;
         end if;
     end process;
--- ArquivosVHDL/RegistradoresPipe/ID_EX_Reg.vhdl
+++ ArquivosVHDL/RegistradoresPipe/ID_EX_Reg.vhdl
@@ -14,16 +14,15 @@ entity ID_EX_Pipeline is
         memWrite_in  : in  std_logic;
         memToReg_in  : in  std_logic;
         regWrite_in  : in  std_logic;
-        branch_in    : in  std_logic;
-        jump_in      : in  std_logic;
         -- Data signals
         Rs1_data_in  : in  std_logic_vector(31 downto 0);
         Rs2_data_in  : in  std_logic_vector(31 downto 0);
         Imm_in : in  std_logic_vector(31 downto 0);
         Rs1_in         : in  std_logic_vector(4 downto 0);
-        Rs2_in         : in  std_logic_vector(4 downto 0); 
+        Rs2_in         : in  std_logic_vector(4 downto 0);
         Rd_in         : in  std_logic_vector(4 downto 0);
-        PC_in         : in std_logic_vector(31 downto 0);
+        funct_3_in      : in  std_logic_vector(2 downto 0);
+        funct_7_in      : in  std_logic_vector(6 downto 0); 
         
         bubble        : in std_logic; -- Input de bubble síncrono
 
@@ -34,8 +33,6 @@ entity ID_EX_Pipeline is
         memWrite_out  : out std_logic;
         memToReg_out  : out std_logic;
         regWrite_out  : out std_logic;
-        branch_out    : out std_logic;
-        jump_out      : out std_logic;
         -- Data outputs
         Rs1_data_out       : out std_logic_vector(31 downto 0);
         Rs2_data_out       : out std_logic_vector(31 downto 0);
@@ -43,7 +40,9 @@ entity ID_EX_Pipeline is
         Rs1_out       : out std_logic_vector(4 downto 0);
         Rs2_out       : out std_logic_vector(4 downto 0);
         Rd_out        : out std_logic_vector(4 downto 0);
-        PC_out        : out std_logic_vector(31 downto 0)
+        PC_out        : out std_logic_vector(31 downto 0);
+        funct_3_out     : out  std_logic_vector(2 downto 0);
+        funct_7_5     : out  std_logic
     );
 end ID_EX_Pipeline;
 
@@ -59,15 +58,12 @@ begin
             memWrite_out   <= '0';
             memToReg_out   <= '0';
             regWrite_out   <= '0';
-            branch_out     <= '0';
-            jump_out       <= '0';
             Rs1_data_out       <= (others => '0');
             Rs2_data_out       <= (others => '0');
             Imm_out       <= (others => '0');
             Rs1_out         <= (others => '0');
             Rs2_out         <= (others => '0');
             Rd_out         <= (others => '0');
-            PC_out         <= (others => '0');
         elsif rising_edge(clk) then
             if bubble = '1' then
                 ALUOp_out      <= (others => '0');
@@ -76,15 +72,12 @@ begin
                 memWrite_out   <= '0';
                 memToReg_out   <= '0';
                 regWrite_out   <= '0';
-                branch_out     <= '0';
-                jump_out       <= '0';
                 Rs1_data_out       <= (others => '0');
                 Rs2_data_out       <= (others => '0');
                 Imm_out       <= (others => '0');
                 Rs1_out         <= (others => '0');
                 Rs2_out         <= (others => '0');
                 Rd_out         <= (others => '0');
-                PC_out         <= (others => '0');
             else
                 -- On clock edge, latch inputs to outputs
                 ALUOp_out      <= ALUOp_in;
@@ -93,15 +86,14 @@ begin
                 memWrite_out   <= memWrite_in;
                 memToReg_out   <= memToReg_in;
                 regWrite_out   <= regWrite_in;
-                branch_out     <= branch_in;
-                jump_out       <= jump_in;
                 Rs1_data_out       <= Rs1_data_in;
                 Rs2_data_out       <= Rs2_data_in;
                 Imm_out       <= Imm_in;
                 Rs1_out         <= Rs1_in;
                 Rs2_out         <= Rs2_in;
                 Rd_out         <= Rd_in;
-                PC_out <= PC_in;
+                funct_3_out <= funct_3_in;
+                funct_7_5 <= funct_7_in(5);
             end if;
         end if;
     end process;
