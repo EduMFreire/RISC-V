06db7eb26ccde3ee77eb7b2ad5fc06099ac20446
PedroRubinstein <pedrovrubinstein.20231@poli.ufrj.br>
2025-06-27 19:20:29 -0300

    Fixing register file.

--- ArquivosVHDL/estagioID/register_file.vhdl
+++ ArquivosVHDL/estagioID/register_file.vhdl
@@ -17,28 +17,39 @@ entity register_file is
 end entity register_file;
 
 architecture behavioral of register_file is
-    -- Define the storage for 32 registers, each 32 bits wide
     type reg_array is array (0 to 31) of std_logic_vector(31 downto 0);
     signal registers : reg_array := (others => (others => '0'));
-
 begin
 
-    -- Read Process: Synchronous to the rising edge (first half of the clock)
+    -- Write Process: Synchronous to the rising edge (first half)
+    write_proc: process(clk, reset)
+    begin
+        if reset = '1' then
+            for i in 0 to 31 loop
+                registers(i) <= (others => '0');
+            end loop;
+        elsif rising_edge(clk) then
+            if reg_write = '1' and rd_addr /= "00000" then
+                registers(to_integer(unsigned(rd_addr))) <= rd_data;
+            end if;
+        end if;
+    end process write_proc;
+
+    -- Read Process: Synchronous to the falling edge (second half)
     read_proc: process(clk, reset)
     begin
         if reset = '1' then
-            -- On reset, clear output data
             rs1_data <= (others => '0');
             rs2_data <= (others => '0');
-        elsif rising_edge(clk) then
-            -- Read rs1. Reading x0 always returns zero.
+        elsif falling_edge(clk) then
+            -- Read rs1
             if rs1_addr = "00000" then
                 rs1_data <= (others => '0');
             else
                 rs1_data <= registers(to_integer(unsigned(rs1_addr)));
             end if;
 
-            -- Read rs2. Reading x0 always returns zero.
+            -- Read rs2
             if rs2_addr = "00000" then
                 rs2_data <= (others => '0');
             else
@@ -46,21 +57,5 @@ begin
             end if;
         end if;
     end process read_proc;
-
-    -- Write Process: Synchronous to the falling edge (second half of the clock)
-    write_proc: process(clk, reset)
-    begin
-        if reset = '1' then
-            -- On reset, clear all registers
-            for i in 0 to 31 loop
-                registers(i) <= (others => '0');
-            end loop;
-        elsif falling_edge(clk) then
-            -- Write only if reg_write is enabled and the destination is not x0
-            if reg_write = '1' and rd_addr /= "00000" then
-                registers(to_integer(unsigned(rd_addr))) <= rd_data;
-            end if;
-        end if;
-    end process write_proc;
 
 end architecture behavioral;
